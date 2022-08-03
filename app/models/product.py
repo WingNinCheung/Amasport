9ea4b76bba9c4b8eba02177659312e84997d5c08
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-08-02 21:59:53 -0700

    completed products table

---
+++ app/models/product.py
@@ -0,0 +1,36 @@
+from .db import db
+
+
+class Product(db.Model):
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    brand = db.Column(db.String(50), nullable=False)
+    about = db.Column(db.String(1000), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    dimensions = db.Column(db.String(100), nullable=False)
+    date_available = db.Column(db.String(100), nullable=False)
+    manufacturer = db.Column(db.String(1000), nullable=False)
+    asin = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(1000), nullable=False)
+
+
+def to_dict(self):
+
+    return {
+        "id": self.id,
+        "name": self.name,
+        "price": self.price,
+        "category": self.category,
+        "brand": self.brand,
+        "about": self.about,
+        "description": self.description,
+        "dimensions": self.dimensions,
+        "date_available": self.date_available,
+        "manufacturer": self.manufacturer,
+        "asin": self.asin,
+        "image": self.image,
+    }
