bb53e70f83c6be899db759b9df32e2d557fc6906
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-08-06 14:42:04 -0700

    added img for qty > 9 and added cart quantity number next to the cart icon in the nav bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "./auth/LogoutButton";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import cartIcon from "../images/cartIcon.png";
+import { getCart, addProduct, updateQuantity } from "../store/cart";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const cart = useSelector((state) => Object.values(state.cart));
+  const userId = sessionUser.id;
+  let totalQuantity = 0;
+
+  cart.forEach((item) => {
+    totalQuantity += item.quantity;
+  });
+
+  useEffect(() => {
+    dispatch(getCart(userId));
+  }, [dispatch]);
 
   return (
     <nav>
@@ -33,6 +46,7 @@ const NavBar = () => {
           <NavLink to="/cart" exact={true} activeClassName="active">
             <img style={{ backgroundColor: "black" }} src={cartIcon}></img>
           </NavLink>
+          <span>{totalQuantity}</span>
           <LogoutButton />
         </section>
       )}
