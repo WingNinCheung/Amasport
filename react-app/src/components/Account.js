a8717ab4fa4574d0971f23d89d079d42abff8a67
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-08-12 15:24:05 -0700

    completed your account section

--- react-app/src/components/Account.js
+++ react-app/src/components/Account.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import "./account.css";
 
 function Account() {
   const [users, setUsers] = useState([]);
@@ -16,21 +17,43 @@ function Account() {
   }, []);
 
   return (
-    <>
-      <h1>Your Account </h1>
-      <div>
-        <div>
-          <NavLink to="/order-history">Your Orders</NavLink>
-          <div>Track, return, or buy things again</div>
-        </div>
-        <div>
-          <NavLink to={`/users/${sessionUser.id}`}>Your Profiles</NavLink>
+    <div className="account-container">
+      <h1 className="account-title">Your Account </h1>
+      <div className="account-item">
+        <div className="acc-order">
           <div>
-            Manage, add, remove user profiles for personalized experiences
+            <img
+              className="order-img"
+              src="https://images-na.ssl-images-amazon.com/images/G/01/x-locale/cs/help/images/gateway/self-service/order._CB660668735_.png"
+              alt="order"
+            ></img>
+          </div>
+          <div className="account-link">
+            <NavLink className="account-link" to="/order-history">
+              <span className="inner-acc">Your Orders</span>
+            </NavLink>
+            <div className="acc-des">Track, return, or buy things again</div>
+          </div>
+        </div>
+        <div className="acc-order">
+          <div>
+            <img
+              className="order-img"
+              src="https://images-na.ssl-images-amazon.com/images/G/01/x-locale/cs/help/images/gateway/self-service/account._CB660668669_.png"
+              alt="profile"
+            ></img>
+          </div>
+          <div className="account-link">
+            <NavLink className="account-link" to={`/users/${sessionUser.id}`}>
+              <span className="inner-acc">Your Profiles</span>
+            </NavLink>
+            <div className="acc-des">
+              Manage, add, remove user profiles for personalized experiences
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
