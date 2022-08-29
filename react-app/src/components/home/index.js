59a7949aff8c85680ea43b30f97ebd030bc9684d
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-08-29 11:45:34 -0700

    styled footer

--- react-app/src/components/home/index.js
+++ react-app/src/components/home/index.js
@@ -105,33 +105,48 @@ function Home() {
       </div>
 
       <div className="footer">
-        <div className="footer-title">
+        <div>
           <div className="tit">Get to Know Me</div>
-          <div className="profile-section">
-            <span className="name">Ricky Cheung</span>
+          <div>
+            <span className="name">Ricky Cheung </span>
             <a
               href="https://wingnincheung.github.io/"
               target="popup"
               className="a-links"
             >
-              <div className="a-div">Portfolio Site</div>
+              <div className="a-div">Portfolio</div>
+            </a>
+            <a
+              href="https://github.com/WingNinCheung/Amasport"
+              target="popup"
+              className="a-links"
+            >
+              <div className="a-div">Github</div>
             </a>
           </div>
         </div>
-        <div className="connect-in">
+        <div>
           <div className="tit">Connect with Me</div>
-          <a href="https://github.com/WingNinCheung" target="popup">
-            <i className="fa-brands fa-github"></i>
+          <a
+            href="https://www.linkedin.com/in/wingnincheung/"
+            target="popup"
+            className="a-links"
+          >
+            <div className="a-div">LinkedIn</div>
           </a>
-          <a href="https://www.linkedin.com/in/wingnincheung/" target="popup">
-            <i className="fa-brands fa-linkedin-in"></i>
-          </a>
-          <a href="mailto:wingnin.cheung415@gmail.com">
-            <i className="fa-solid fa-envelope"></i>
+          <a href="mailto:wingnin.cheung415@gmail.com" className="a-links">
+            <div className="a-div">Email</div>
           </a>
         </div>
-        <div className="tech-list">
-          <div className="tit">Technologies</div>
+        <div className="tech-container">
+          <div className="tit">Technologies Used</div>
+          <div className="a-div">Javascript</div>
+          <div className="a-div">React / Redux</div>
+          <div className="a-div">Python / Flask</div>
+          <div className="a-div">SQL Alchemy</div>
+          <div className="a-div">PostgresSQL</div>
+          <div className="a-div">HTML5 / CSS3</div>
+          <div className="a-div">Docker</div>
         </div>
       </div>
     </div>
