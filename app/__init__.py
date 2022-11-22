7c436db2e6b0f33bb21a7ece7b78207471542b72
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-11-21 16:27:53 -0800

    tried send_from_directory

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import (
     request,
     session,
     redirect,
+    send_from_directory,
 )
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -83,5 +84,5 @@ def inject_csrf_token(response):
 @app.route("/<path:path>")
 def react_root(path):
     if path == "favicon.ico":
-        return app.send_static_file("favicon.ico")
+        return app.send_from_directory("public", "favicon.ico")
     return app.send_static_file("index.html")
